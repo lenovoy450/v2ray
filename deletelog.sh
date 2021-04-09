e3b41ade7b2e5f850126b542d75e7107d1dca052
goudaozhu <74314877+goudaozhu@users.noreply.github.com>
2021-04-10 03:01:22 +0700

    Add files via upload

---
+++ deletelog.sh
@@ -0,0 +1,26 @@
+rm -rf /var/log/*.gz
+rm -rf /var/log/*.1
+rm -rf /var/log/*.2
+rm -rf /var/log/*.3
+echo "" > /var/log/dmesg
+echo "" > /var/log/kern.log
+echo "" > /var/log/messages
+echo "" > /var/log/daemon.log
+echo "" > /var/log/syslog
+cat /dev/null > /var/log/wtmp
+cat /dev/null > /var/log/btmp
+cat /dev/null > /var/log/lastlog
+cat /dev/null > /var/log/secure
+cat /dev/null > /var/log/boot.log
+cat /dev/null > /var/log/cron
+cat /dev/null > /var/log/dmesg
+cat /dev/null > /var/log/firewalld
+cat /dev/null > /var/log/maillog
+cat /dev/null > /var/log/messages
+cat /dev/null > /var/log/daemon.log
+cat /dev/null > /var/log/spooler
+cat /dev/null > /var/log/syslog
+cat /dev/null > /var/log/tallylog
+cat /dev/null > /var/log/yum.log
+cat /dev/null > /var/log/auth.log
+echo "" > /root/.bash_history
